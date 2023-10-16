abc2a173085da2ebcb8a5ed259078f5b1a7a9c41
timothy-r <tim.rodger@gmail.com>
2023-10-16 23:29:42 +0100

    updates cols post processor

--- aviva_pensions/processors/add_columns_post_processor.py
+++ aviva_pensions/processors/add_columns_post_processor.py
@@ -9,54 +9,76 @@ class AddColumnsPostProcessor(PostProcessorInterface):
     """
 
     def __init__(self, key:str, columns:list[str], data_provider:DataProviderInterface) -> None:
-        self._columns = columns
+        self._key_name = key
+        self._columns = {key: '' for key in columns}
         self._data_provider = data_provider
         self._source_data = {}
-        self._key_name = key
 
-
-    def process(self, row: dict) -> dict:
+    def process(self, target_row: dict) -> dict:
         """
-            add named columns from source csv to the data
-            keyed on sedol
+            add named columns from source csv to the data if they are not set
+            add empty col values if the target_row key is not in the source data
+            it might be clearer to generate a dict of values to add to target
+            then use update to add those new values
         """
         if len(self._source_data) == 0:
             self._source_data = self._data_provider.read_data()
 
-        # if the key is not in the input then add empty columns
-        if not self._key_name in row:
-            print("key {} not found in {}".format(self._key_name, row))
-            row = self._add_empty_cols(row=row)
-            return row
+        try:
+            key = self._get_key_value_from_target(target_row=target_row)
+        except KeyError:
+            key = None
 
-        # get the id to read from the data source
-        key = str(row[self._key_name])
+        source_row = self._get_source_row(key=key, target_row=target_row)
+
+        additions = self._calculate_additional_cols(target_row=target_row, source_row=source_row)
+
+        target_row.update(additions)
+
+        return target_row
+
+    def _get_key_value_from_target(self, target_row:dict) -> str:
+        # get the key value to read from the data source
+
+        key = str(target_row[self._key_name])
 
         # remove leading 0s from the ids
         if key[0] == '0':
             key = str(int(key))
 
-        if key in self._source_data:
+        return key
 
-            source_data_row = self._source_data[key]
-
-            for col in self._columns:
-                if col in source_data_row:
-                    if col in row and row[col] == '':
-                        row[col] = source_data_row[col]
-                    else:
-                        row[col] = source_data_row[col]
-                else:
-                    row[col] = ''
-        else:
-            row = self._add_empty_cols(row=row)
-            print("id {}\n not found in {}".format(key, self._source_data.keys()))
-
-        return row
-
-    def _add_empty_cols(self, row:dict) -> dict:
-        """ fill out the parameter dict with all the configured col names and empty values
+    def _get_source_row(self, key:str, target_row:dict) -> dict:
+        """ Returns the source row data to add to the target
         """
+        if not self._key_name in target_row:
+            print("key {} not found in {}".format(self._key_name, target_row))
+            # add empty columns
+            return self._columns
+
+        if key in self._source_data:
+            # add columns from source
+            return self._source_data[key]
+        else:
+            print("id {}\n not found in {}".format(key, self._source_data.keys()))
+            # add empty columns
+            return self._columns
+
+    def _calculate_additional_cols(self, target_row:dict, source_row:dict) -> dict:
+        """ generate a dict of keys & values to add to target from source
+        """
+        result = {}
         for col in self._columns:
-            row[col] = ''
-        return row
+
+            if col in source_row:
+                # if the col exists but is empty then set the value
+                if col in target_row and target_row[col] == '':
+                    result[col] = source_row[col]
+                elif not col in target_row:
+                    # if the col does not exist in target then just set the value
+                    result[col] = source_row[col]
+            else:
+                if not col in target_row:
+                    result[col] = ''
+
+        return result
--- aviva_pensions/tests/add_columns_post_processor_test.py
+++ aviva_pensions/tests/add_columns_post_processor_test.py
@@ -20,7 +20,7 @@ class AddColumnsPostProcessorTest(unittest.TestCase):
         _processor = AddColumnsPostProcessor(key=key, columns=cols, data_provider=data_provider)
 
         row = {'SEDOL':'B5M9Z37'}
-        results = _processor.process(row=row)
+        results = _processor.process(target_row=row)
 
         self.assertTrue(4 == len(results))
         self.assertTrue('B5M9Z37' == results['SEDOL'])
@@ -41,7 +41,7 @@ class AddColumnsPostProcessorTest(unittest.TestCase):
         _processor = AddColumnsPostProcessor(key=key, columns=cols,data_provider=data_provider)
 
         row = {'SEDOL':'03346251'}
-        results = _processor.process(row=row)
+        results = _processor.process(target_row=row)
 
         self.assertTrue(4 == len(results))
         self.assertTrue('03346251' == results['SEDOL'])
@@ -62,10 +62,94 @@ class AddColumnsPostProcessorTest(unittest.TestCase):
         _processor = AddColumnsPostProcessor(key=key, columns=cols,data_provider=data_provider)
 
         row = {'SEDOL':'0123456'}
-        results = _processor.process(row=row)
+        results = _processor.process(target_row=row)
 
         self.assertTrue(4 == len(results))
         self.assertTrue('0123456' == results['SEDOL'])
+        self.assertTrue('' == results['Charge'])
+        self.assertTrue('' == results['MS ratings'])
+        self.assertTrue('' == results['FT ratings'])
+
+    def test_doesnt_overwrite_cols(self) -> None:
+        key = 'SEDOL'
+        cols = ['Charge','MS ratings', 'FT ratings']
+        data = [
+            {'SEDOL':'B5M9Z37', 'Charge':'0.17%', 'MS ratings':'4', 'FT ratings':'3'},
+            {'SEDOL':'B7M1101', 'Charge':'0.20%', 'MS ratings':'5', 'FT ratings':'2'}
+        ]
+
+        data_provider = ListToDictDataProvider(key=key, reader=data)
+        _processor = AddColumnsPostProcessor(key=key, columns=cols, data_provider=data_provider)
+
+        row = {'SEDOL':'B5M9Z37', 'Charge':'0.55%'}
+
+        results = _processor.process(target_row=row)
+
+        self.assertTrue(4 == len(results))
+        self.assertTrue('B5M9Z37' == results['SEDOL'])
+        self.assertTrue('0.55%' == results['Charge'])
+        self.assertTrue('4' == results['MS ratings'])
+        self.assertTrue('3' == results['FT ratings'])
+
+    def test_doesnt_overwrite_cols_for_missing_key(self) -> None:
+        key = 'SEDOL'
+        cols = ['Charge','MS ratings', 'FT ratings']
+        data = [
+            {'SEDOL':'B5M9Z37', 'Charge':'0.17%', 'MS ratings':'4', 'FT ratings':'3'},
+            {'SEDOL':'B7M1101', 'Charge':'0.20%', 'MS ratings':'5', 'FT ratings':'2'}
+        ]
+
+        data_provider = ListToDictDataProvider(key=key, reader=data)
+        _processor = AddColumnsPostProcessor(key=key, columns=cols, data_provider=data_provider)
+
+        row = {'SEDOL':'B5M9000', 'Charge':'0.55%'}
+
+        results = _processor.process(target_row=row)
+
+        self.assertTrue(4 == len(results))
+        self.assertTrue('B5M9000' == results['SEDOL'])
+        self.assertTrue('0.55%' == results['Charge'])
+        self.assertTrue('' == results['MS ratings'])
+        self.assertTrue('' == results['FT ratings'])
+
+    def test_sets_missing_cols_from_source(self) -> None:
+        key = 'SEDOL'
+        cols = ['Charge','MS ratings', 'FT ratings']
+        data = [
+            {'SEDOL':'B5M9Z37', 'Charge':'0.17%', 'MS ratings':'4', 'FT ratings':'3'},
+            {'SEDOL':'B7M1101', 'Charge':'0.20%', 'FT ratings':'2'}
+        ]
+
+        data_provider = ListToDictDataProvider(key=key, reader=data)
+        _processor = AddColumnsPostProcessor(key=key, columns=cols, data_provider=data_provider)
+
+        row = {'SEDOL':'B7M1101'}
+
+        results = _processor.process(target_row=row)
+
+        self.assertTrue(4 == len(results))
+        self.assertTrue('B7M1101' == results['SEDOL'])
+        self.assertTrue('0.20%' == results['Charge'])
+        self.assertTrue('' == results['MS ratings'])
+        self.assertTrue('2' == results['FT ratings'])
+
+    def test_supports_missing_target_key(self) -> None:
+        key = 'SEDOL'
+        cols = ['Charge','MS ratings', 'FT ratings']
+        data = [
+            {'SEDOL':'B5M9Z37', 'Charge':'0.17%', 'MS ratings':'4', 'FT ratings':'3'},
+            {'SEDOL':'B7M1101', 'Charge':'0.20%', 'FT ratings':'2'}
+        ]
+
+        data_provider = ListToDictDataProvider(key=key, reader=data)
+        _processor = AddColumnsPostProcessor(key=key, columns=cols, data_provider=data_provider)
+
+        row = {'XYZ':'123'}
+
+        results = _processor.process(target_row=row)
+
+        self.assertTrue(4 == len(results))
+        self.assertTrue('123' == results['XYZ'])
         self.assertTrue('' == results['Charge'])
         self.assertTrue('' == results['MS ratings'])
         self.assertTrue('' == results['FT ratings'])
